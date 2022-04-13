1ea10a1fb1bbddb2372827df44c6f9140e9e82ed
Kim Kennedy <kim@empireofwe.org>
2022-04-13 14:10:59 -0600

    Update name of orginal list to my_list

--- 3_lists/main.py
+++ 3_lists/main.py
@@ -1,33 +1,33 @@
 name = 'Kim'
-shit = [42, 'tank', 3.14]
+my_list = [42, 'tank', 3.14]
 
 name2 = 'tank'
 
 name3 = 'kevin'
 
 print(name,name2,name3)
-print(shit)
-print(shit[0])
-print(shit[1])
-print(shit[2])
+print(my_list)
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
 
-print(len(shit))
+print(len(my_list))
 
-print(type(shit))
+print(type(my_list))
 
-print(shit[-3])
+print(my_list[-3])
 
-print(shit[1:3])
+print(my_list[1:3])
 
-print(shit[1:])
+print(my_list[1:])
 
-if 52 in shit:
-    print('tank is in shit')
+if 52 in my_list:
+    print('tank is in my_list')
 else:
-    print('item is not in this list')
+    print('item is not in this my_list')
 
-shit[0] = 99
-print(shit)
+my_list[0] = 99
+print(my_list)
 
 attack_test = ['max', 'min', 'geartax']
 attack_test.sort()
@@ -52,4 +52,4 @@ print(attack_test_2)
 attack_test = [11, 75, 888, 43]
 attack_test_2 = [12, 85, 788, 49]
 attack_test.extend(attack_test_2)   
-print(attack_test)
+print(attack_test) 
